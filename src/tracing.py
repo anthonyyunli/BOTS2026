a5853dcb8ff6ef7eda808aed0a4ddba01f86784a
sanikakel <sanika.k28@gmail.com>
2026-09-12 11:58:25 -0400

    Update tracing.py

--- src/tracing.py
+++ src/tracing.py
@@ -1,1 +1,14 @@
+'''
+Once we find a possible branch, we need to determine if it is an ACTUAL daughter artery.
 
+The branch needs to be traceable for at least 5 mm beyond the aortic wall to be eligible.
+
+Potentially use:
+- connected components
+- vesselness filters
+- region growing
+- graph search
+- centerline extraction
+- intensity-based path following
+
+'''
