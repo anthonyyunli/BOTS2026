8d91d6b04bfa94c0aa560004ee2972ce5337dfa5
sanikakel <sanika.k28@gmail.com>
2026-09-12 11:51:37 -0400

    Update io.py

--- src/io.py
+++ src/io.py
@@ -1,1 +1,15 @@
+'''
+handles medical imaging files- use SimpleITK
 
+So this file handles things like:
+image = sitk.ReadImage(...)
+mask = sitk.ReadImage(...)
+
+array = sitk.GetArrayFromImage(image)
+and importantly, keeps track of:
+spacing
+origin
+direction
+physical coordinates
+
+'''
