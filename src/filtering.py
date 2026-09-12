4be96f58578b98294b9ac91e76544930f00f407d
sanikakel <sanika.k28@gmail.com>
2026-09-12 12:02:25 -0400

    Update filtering.py

--- src/filtering.py
+++ src/filtering.py
@@ -1,1 +1,12 @@
+'''
+Algorithm will probably initially find too many things (false positives)
 
+We need to eliminate:
+- noise
+- unrelated vessels
+- artifacts
+- vessels branching from another daughter
+- cropping edges
+- duplicate detections
+
+'''
