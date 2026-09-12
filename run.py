6bb5b25d236754d9fb09b90133a8696794811088
Ziena Elsabban <97271505+527-Ziena@users.noreply.github.com>
2026-09-12 14:56:53 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
 import argparse  # for the hiddentests where they uppload a ct scan to check code 
+from src.io import load_nifti_image, get_image_array
 def main():
     # will understand the information given by the user 
     parser = argparse.ArgumentParser()  
     # get the CT file 
     parser.add_argument("--image", required=True)
     # get the aorta mask 
-    parser.add_argument("--aorta-mask". required=True)
+    parser.add_argument("--aorta-mask", required=True)
     # get the location where we will save our prediciton 
     parser.add_argument("--output", required=True)
     # to save the result as json 
@@ -14,9 +15,15 @@ def main():
     image_path = args.image  
     mask_path = args.aorta_mask 
     output_path = args.output 
-    
-    # 1. Load CT
-    # 2. Load aorta mask
+    # load CT and aorta mask 
+    ct_image = load_nifti_image(image_path)
+    mask_image= load_nifti_image(mask_path)
+    ct_array= get_image_array(ct_image)
+    mask_array= get_image_array(mask_image) 
+    # to test 
+    print("CT shape:", ct_array.shape)
+    print("Mask shape:", mask_array.shape) 
+
     # 3. Detect branches
     # 4. Analyze branches
     # 5. Generate JSON
