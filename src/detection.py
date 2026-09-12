567656d2964728fe68a1faae79fa4a6e1706e2fa
sanikakel <sanika.k28@gmail.com>
2026-09-12 11:55:02 -0400

    Update detection.py

--- src/detection.py
+++ src/detection.py
@@ -1,1 +1,9 @@
+'''
+Using the aorta mask:
+- Find aortic surface
+- Look around the surface
+- Find bright vessel-like structures
+- Find structures connected to aorta
+- These could be branches
 
+'''
