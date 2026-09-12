3d02981fdaff919914bc243a560b5048d646c3ab
sanikakel <sanika.k28@gmail.com>
2026-09-12 11:49:48 -0400

    Create run.py

---
+++ run.py
@@ -0,0 +1,10 @@
+def main():
+    # 1. Load CT
+    # 2. Load aorta mask
+    # 3. Detect branches
+    # 4. Analyze branches
+    # 5. Generate JSON
+    # 6. Save prediction
+
+if __name__ == "__main__":
+    main()
